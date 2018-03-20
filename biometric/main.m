dfe85c7316dda49b00171589c241d1608c6ab8f2
TM <tmanning@duo.com>
2018-03-20 18:07:24 -0500

    Initial Commit

---
+++ biometric/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  biometric
+//
+//  Created by TM on 3/20/18.
+//  Copyright © 2018 Weaponize. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

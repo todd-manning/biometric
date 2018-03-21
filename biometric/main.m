121352891d61c5bc4c0455cc7ca0ac5bbfed55e1
TM <tmanning@duo.com>
2018-03-21 10:24:41 -0500

    First POC created using sources found online. These sources are listed in main.m. This proof of concept attempts to authenticate the user using LocalAuthentication framework. The build target is macOS 10.13, so this code only handles TouchID and not FaceID. The command line tool will provide a reason for the failure as given by the framework.

--- biometric/main.m
+++ biometric/main.m
@@ -2,16 +2,72 @@
 //  main.m
 //  biometric
 //
-//  Created by TM on 3/20/18.
-//  Copyright © 2018 Weaponize. All rights reserved.
-//
+//  Using examples from
+//  - https://developer.apple.com/documentation/localauthentication?language=objc
+//  - sudo-touchid https://github.com/mattrajca/sudo-touchid
+
 
 #import <Foundation/Foundation.h>
+#import <LocalAuthentication/LocalAuthentication.h>
+#import <stdio.h>
+
+typedef enum {
+    kTouchIDResultAllowed,
+    kTouchIDResultNone,
+    kTouchIDResultFailed
+} TouchIDResult;
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        LAContext *myContext = [[LAContext alloc] init];
+        NSString *myLocalizedReasonString = @"authenticate";
+
+        __block NSError *authError = nil;
+        __block TouchIDResult result = kTouchIDResultNone;
+        
+        
+        if ([myContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError]) {
+            // biometryType is only set after calling canEvaluatePolicy
+            if (@available(macOS 10.13.2, *)) {
+            
+                switch(myContext.biometryType) {
+                    case LABiometryTypeNone:
+                        break;
+                    case LABiometryTypeTouchID:
+                        break;
+
+                    // currently only iOS supports FaceID -- build warning turned off for case statement check
+                    //case LABiometryTypeFaceID:
+                    //    break;
+                }
+
+            } 
+
+            [myContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                      localizedReason:myLocalizedReasonString
+                                reply:^(BOOL success, NSError *error) {
+                                    result = success ? kTouchIDResultAllowed : kTouchIDResultFailed;
+                                    authError = error;
+                                    CFRunLoopWakeUp(CFRunLoopGetCurrent());
+                                }];
+            
+            while (result == kTouchIDResultNone) {
+                CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
+            }
+            
+            if (result == kTouchIDResultAllowed) {
+                printf("Biometric authentication succeeded\n");
+            } else {
+                printf("Biometric authentication failed, errorcode==%ld userinfo=%s\n", (long)authError.code, [authError.localizedDescription UTF8String]);
+            }
+            
+        } else {
+            // Could not evaluate policy
+            printf("Could not perform biometric authentication...errorcode==%ld userinfo=%s\n", (long)authError.code, [authError.localizedDescription UTF8String]);
+        }
+        
+        return (int)authError.code;
+
     }
-    return 0;
 }
